9d3963624221df7cac4f7e9a10dde3bb59770691
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-14 22:10:37 +0530

    Gesture Detector

--- lib/widgets/custombutton.dart
+++ lib/widgets/custombutton.dart
@@ -5,25 +5,29 @@ class CustomButton extends StatelessWidget {
 final Color accentColor;
 final Color mainColor;
 final String text;
-CustomButton({this.accentColor,this.text,this.mainColor});
+final Function onpress;
+CustomButton({this.accentColor,this.text,this.mainColor,this.onpress});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: accentColor,
-        ),
-        color: mainColor,
-      borderRadius: BorderRadius.circular(50)
-      ),
-      width: MediaQuery.of(context).size.width * 0.6,
-      padding: EdgeInsets.all(15),
-      child: Center(
-        child: Text(
-            text.toUpperCase(),
-            style: TextStyle(fontFamily: 'Poppins',color: accentColor),
+    return GestureDetector(
+        onTap:onpress,
+          child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: accentColor,
           ),
+          color: mainColor,
+        borderRadius: BorderRadius.circular(50)
+        ),
+        width: MediaQuery.of(context).size.width * 0.6,
+        padding: EdgeInsets.all(15),
+        child: Center(
+          child: Text(
+              text.toUpperCase(),
+              style: TextStyle(fontFamily: 'Poppins',color: accentColor),
+            ),
+        ),
       ),
     );
   }
