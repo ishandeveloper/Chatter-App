826ffa669f0dbb9911ae728bc429ffb54db668e2
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-14 22:35:56 +0530

    custom textfield

---
+++ lib/widgets/customtextinput.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomTextInput extends StatelessWidget {
+  final String hintText;
+  final IconData leading;
+
+  CustomTextInput({this.hintText, this.leading});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      padding: EdgeInsets.only(left: 10),
+      width: MediaQuery.of(context).size.width * 0.70,
+      child: TextField(
+        onChanged: (value) {},
+        onSubmitted: (value) {},
+        autofocus: false,
+        decoration: InputDecoration(
+          icon: Icon(
+            leading,
+            color: Colors.deepPurple,
+          ),
+          border: InputBorder.none,
+          hintText: hintText,
+          hintStyle: TextStyle(fontFamily: 'Poppins'),
+        ),
+      ),
+    );
+  }
+}
