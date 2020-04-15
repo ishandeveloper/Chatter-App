473bdc411bbbd0e65211863cd4d5d2b87a9e7443
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-15 19:39:01 +0530

    Retrieving Messages from FireStore

--- lib/pages/chatterScreen.dart
+++ lib/pages/chatterScreen.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../constants.dart';
 
 class ChatterScreen extends StatefulWidget {
@@ -11,8 +11,10 @@ class ChatterScreen extends StatefulWidget {
 class _ChatterScreenState extends State<ChatterScreen> {
   String username = 'User';
   String email = 'user@example.com';
-
+  String messageText;
   final _auth = FirebaseAuth.instance;
+  final _firestore = Firestore.instance;
+
   FirebaseUser loggedInUser;
   void getCurrentUser() async {
     try {
@@ -28,113 +30,132 @@ class _ChatterScreenState extends State<ChatterScreen> {
       print(e);
     }
   }
+  void getMessages()async{
+    final messages=await _firestore.collection('messages').getDocuments();
+    for(var message in messages.documents){
+      print(message.data);
+    }
+  }
+
+  
+
 
   @override
   void initState() {
     super.initState();
     getCurrentUser();
+    getMessages();
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          bottom: PreferredSize(
-            preferredSize: Size(25, 10),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.deepPurple[300],
-                // borderRadius: BorderRadius.circular(20)
-              ),
-              constraints: BoxConstraints.expand(height: 5),
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.deepPurple),
+        elevation: 0,
+        bottom: PreferredSize(
+          preferredSize: Size(25, 10),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              // borderRadius: BorderRadius.circular(20)
             ),
-          ),
-          backgroundColor: Colors.deepPurple[900],
-          // leading: Padding(
-          //   padding: const EdgeInsets.all(12.0),
-          //   child: CircleAvatar(backgroundImage: NetworkImage('https://cdn.clipart.email/93ce84c4f719bd9a234fb92ab331bec4_frisco-specialty-clinic-vail-health_480-480.png'),),
-          // ),
-          title: Row(
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'Chatter',
-                    style: TextStyle(fontFamily: 'Poppins', fontSize: 16),
-                  ),
-                  Text('by ishandeveloper',
-                      style: TextStyle(fontFamily: 'Poppins', fontSize: 8))
-                ],
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            GestureDetector(
-              child: Icon(Icons.more_vert),
-            )
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              UserAccountsDrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.deepPurple[900],
-                ),
-                accountName: Text(username),
-                accountEmail: Text(email),
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      "https://cdn.clipart.email/93ce84c4f719bd9a234fb92ab331bec4_frisco-specialty-clinic-vail-health_480-480.png"),
-                ),
-                onDetailsPressed: () {},
-              ),
-              ListTile(
-                leading: Icon(Icons.exit_to_app),
-                title: Text("Logout"),
-                subtitle: Text("Sign out of this account"),
-                onTap: () async{
-                  await _auth.signOut();
-                  Navigator.pushReplacementNamed(context, '/');
-                },
-              ), 
-            ],
+            constraints: BoxConstraints.expand(height: 5),
           ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        backgroundColor: Colors.white10,
+        // leading: Padding(
+        //   padding: const EdgeInsets.all(12.0),
+        //   child: CircleAvatar(backgroundImage: NetworkImage('https://cdn.clipart.email/93ce84c4f719bd9a234fb92ab331bec4_frisco-specialty-clinic-vail-health_480-480.png'),),
+        // ),
+        title: Row(
           children: <Widget>[
-            Container(
-              decoration: kMessageContainerDecoration,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {
-                        //Do something with the user input.
-                      },
-                      decoration: kMessageTextFieldDecoration,
-                    ),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      //Implement send functionality.
-                    },
-                    child: Text(
-                      'Send',
-                      style: kSendButtonTextStyle,
-                    ),
-                  ),
-                ],
-              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Chatter',
+                  style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 16,
+                      color: Colors.deepPurple),
+                ),
+                Text('by ishandeveloper',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 8,
+                        color: Colors.deepPurple))
+              ],
             ),
           ],
         ),
+        actions: <Widget>[
+          GestureDetector(
+            child: Icon(Icons.more_vert),
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.deepPurple[900],
+              ),
+              accountName: Text(username),
+              accountEmail: Text(email),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://cdn.clipart.email/93ce84c4f719bd9a234fb92ab331bec4_frisco-specialty-clinic-vail-health_480-480.png"),
+              ),
+              onDetailsPressed: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text("Logout"),
+              subtitle: Text("Sign out of this account"),
+              onTap: () async {
+                await _auth.signOut();
+                Navigator.pushReplacementNamed(context, '/');
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            decoration: kMessageContainerDecoration,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    onChanged: (value) {
+                      messageText = value;
+                    },
+                    decoration: kMessageTextFieldDecoration,
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    _firestore.collection('messages').add({
+                      'sender': username,
+                      'text': messageText,
+                      'senderemail': email
+                    });
+                  },
+                  child: Text(
+                    'Send',
+                    style: kSendButtonTextStyle,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
