dc16132a6b7f54083a89775d2917977da254f914
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-15 18:59:38 +0530

    processing spinner

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,105 +1,161 @@
 import 'package:chat_app/widgets/custombutton.dart';
 import 'package:chat_app/widgets/customtextinput.dart';
+import 'package:edge_alert/edge_alert.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
-class ChatterLogin extends StatelessWidget {
+class ChatterLogin extends StatefulWidget {
+  @override
+  _ChatterLoginState createState() => _ChatterLoginState();
+}
+
+class _ChatterLoginState extends State<ChatterLogin> {
+  String email;
+  String password;
+  bool loggingin = false;
+  final _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor: Colors.transparent,
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          // margin: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.2),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Hero(
-                  tag: 'heroicon',
-                  child: Icon(
-                    Icons.textsms,
-                    size: 120,
-                    color: Colors.deepPurple[900],
+    return ModalProgressHUD(
+      inAsyncCall: loggingin,
+      child: Scaffold(
+        // backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            // margin: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.2),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Hero(
+                    tag: 'heroicon',
+                    child: Icon(
+                      Icons.textsms,
+                      size: 120,
+                      color: Colors.deepPurple[900],
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.02,
-                ),
-                Hero(
-                  tag:'HeroTitle',
-                                  child: Text(
-                    'Chatter',
-                    style: TextStyle(
-                        color: Colors.deepPurple[900],
-                        fontFamily: 'Poppins',
-                        fontSize: 26,
-                        fontWeight: FontWeight.w700),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
                   ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.01,
-                ),
-                // Text(
-                //   "World's most private chatting app".toUpperCase(),
-                //   style: TextStyle(
-                //       fontFamily: 'Poppins',
-                //       fontSize: 12,
-                //       color: Colors.deepPurple),
-                // ),
-                CustomTextInput(
-                  hintText: 'Username',
-                  leading: Icons.supervised_user_circle,
-                  obscure: false,
-                ),
-                SizedBox(
-                  height: 0,
-                ),
-                CustomTextInput(
-                  hintText: 'Password',
-                  leading: Icons.lock,
-                  obscure: true,
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Hero(
-                  tag:'loginbutton',
-                                  child: CustomButton(
-                    text: 'login',
-                    accentColor: Colors.white,
-                    mainColor: Colors.deepPurple,
-                    onpress: (){
-                      Navigator.pushReplacementNamed(context, '/chat');
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacementNamed(context, '/signup');
-                    },
+                  Hero(
+                    tag: 'HeroTitle',
                     child: Text(
-                      'or create an account',
+                      'Chatter',
                       style: TextStyle(
+                          color: Colors.deepPurple[900],
                           fontFamily: 'Poppins',
-                          fontSize: 12,
-                          color: Colors.deepPurple),
-                    )),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.1,
-                ),
-                Hero(
-                  tag:'footer',
-                                  child: Text(
-                    'Made with ♥ by ishandeveloper',
-                    style: TextStyle(fontFamily: 'Poppins'),
+                          fontSize: 26,
+                          fontWeight: FontWeight.w700),
+                    ),
                   ),
-                )
-              ],
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.01,
+                  ),
+                  // Text(
+                  //   "World's most private chatting app".toUpperCase(),
+                  //   style: TextStyle(
+                  //       fontFamily: 'Poppins',
+                  //       fontSize: 12,
+                  //       color: Colors.deepPurple),
+                  // ),
+                  CustomTextInput(
+                    hintText: 'Email',
+                    leading: Icons.mail,
+                    obscure: false,
+                    keyboard: TextInputType.emailAddress,
+                    userTyped: (val) {
+                      email = val;
+                    },
+                  ),
+                  SizedBox(
+                    height: 0,
+                  ),
+                  CustomTextInput(
+                    hintText: 'Password',
+                    leading: Icons.lock,
+                    obscure: true,
+                    userTyped: (val) {
+                      password = val;
+                    },
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Hero(
+                    tag: 'loginbutton',
+                    child: CustomButton(
+                      text: 'login',
+                      accentColor: Colors.white,
+                      mainColor: Colors.deepPurple,
+                      onpress: () async {
+                        if (password != null && email != null) {
+                          setState(() {
+                            loggingin = true;
+                          });
+                          try {
+                            final loggedUser =
+                                await _auth.signInWithEmailAndPassword(
+                                    email: email, password: password);
+                            if (loggedUser != null) {
+                              setState(() {
+                                loggingin = false;
+                              });
+                              Navigator.pushNamed(context, '/chat');
+                            }
+                          } catch (e) {
+                            setState(() {
+                                loggingin = false;
+                              });
+                            EdgeAlert.show(context,
+                                title: 'Login Failed',
+                                description: e.toString(),
+                                gravity: EdgeAlert.BOTTOM,
+                                icon: Icons.error,
+                                backgroundColor: Colors.deepPurple[900]);
+                          }
+                        } else {
+                          EdgeAlert.show(context,
+                              title: 'Uh oh!',
+                              description:
+                                  'Please enter the email and password.',
+                              gravity: EdgeAlert.BOTTOM,
+                              icon: Icons.error,
+                              backgroundColor: Colors.deepPurple[900]);
+                        }
+                        // Navigator.pushReplacementNamed(context, '/chat');
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacementNamed(context, '/signup');
+                      },
+                      child: Text(
+                        'or create an account',
+                        style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 12,
+                            color: Colors.deepPurple),
+                      )),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                  Hero(
+                    tag: 'footer',
+                    child: Text(
+                      'Made with ♥ by ishandeveloper',
+                      style: TextStyle(fontFamily: 'Poppins'),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
